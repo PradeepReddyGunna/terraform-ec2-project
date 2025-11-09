efd3db9caa222bc479d9bc21bdad9e136d2f6d84
PradeepReddyGunna <126529079+PradeepReddyGunna@users.noreply.github.com>
2025-11-09 14:33:48 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     aws = {
@@ -12,10 +13,24 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Create a simple EC2 instance in the default VPC
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get first subnet from default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Create EC2 instance in the default subnet
 resource "aws_instance" "terraform_demo" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
+  subnet_id                   = data.aws_subnets.default.ids[0]
   associate_public_ip_address  = true
 
   tags = {
