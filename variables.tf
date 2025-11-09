238376679808af683627027671c0adac32789015
PradeepReddyGunna <126529079+PradeepReddyGunna@users.noreply.github.com>
2025-11-09 14:40:07 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-east-1"  # region with default VPC
+  default     = "us-east-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for EC2 instance"
   type        = string
-  default     = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  default     = "ami-0c02fb55956c7d316"
 }
 
 variable "instance_type" {
-  description = "Type of EC2 instance"
+  description = "EC2 instance type"
   type        = string
   default     = "t2.micro"
 }
