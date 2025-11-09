04ef40f78ec389114b616b4ed4f247012a644264
PradeepReddyGunna <126529079+PradeepReddyGunna@users.noreply.github.com>
2025-11-09 14:21:56 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,13 @@
+# outputs.tf
+# Output information after apply
+
 output "instance_public_ip" {
-  value = aws_instance.terraform_demo.public_ip
+  description = "Public IP of created EC2 instance"
+  value       = aws_instance.terraform_demo.public_ip
 }
 
 output "instance_id" {
-  value = aws_instance.terraform_demo.id
+  description = "EC2 instance ID"
+  value       = aws_instance.terraform_demo.id
 }
-
 
